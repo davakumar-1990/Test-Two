2806f6623346a4fd881ce258b0ef474da74968b8
davakumar-1990 <davasupamani@gmail.com>
2022-12-26 10:59:41 +0530

    Test Two Project

---
+++ src/org/test/Sample.java
@@ -0,0 +1,34 @@
+package org.test;
+
+public class Sample {
+	
+	private void mano() {
+		System.out.println("Name");
+	}
+	private void number() {
+		System.out.println("1516416");
+	}
+	private void Gender() {
+		System.out.println("Male");
+	}
+	private void area() {
+		System.out.println("Chennai");
+	}
+	public static void main(String[] args) {
+		Sample a=new Sample();
+		a.mano();
+		a.number();
+		a.Gender();
+		a.area();
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
